7b5e1729f818255662e934002634deac4559db04
Van Chuong <134tutu431@gmail.com>
2024-06-05 08:31:23 +0700

    push data message

--- src/index.js
+++ src/index.js
@@ -100,6 +100,68 @@ app.get('/join-room', async (req, res) => {
     }
 });
 
+const sdk = require('node-appwrite');
+const { channel } = require("diagnostics_channel");
+
+// Init SDK
+const client = new sdk.Client();
+
+const messaging = new sdk.Messaging(client);
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')    // Your API Endpoint
+    .setProject('663b932300030cf0a819')                    // Your project ID
+    .setKey('8303b36782ec09cf2bd02f44f41b04687567cc47d85cbfd26204248bfa381abc625246262793c7bcbb37891bab9aa6a146d2760d053b607b300b6da306f429b401ee2dad31b541e28794b3ac3d2a86e9a0b063843c9068b5c8c58de3065bb9efb025977926ce306d389b244d6794357dcb81878f8193b2f4c3183e3aab5bc5cc')    // Your secret API key
+;
+
+
+
+app.get('/push', async (req, res) => {
+  //   const targets = [
+  //     'eLgzSbgrS5qALtUAezu7cs:APA91bEy4FApIBqxFgybrrxtpBC2dRgkGIIIr032ke1HQgRQpaqTxRtghOZUg3ElWMqDTOchqIc9MX60PrtwcqO7CBdGSktOpCEqBQpLW1_VkW68EXlT4oHeMuhJ6LkZ5PV6XvZ7RiPW', // Thay thế bằng token của người dùng 1
+  //     'token2', // Thay thế bằng token của người dùng 2
+  //     // ... thêm các token của các người dùng khác
+  // ];
+    try{
+    //   await messaging.createPush(
+    //     sdk.ID.unique(), // messageId
+    //     'OK',      // title
+    //     'body',       // body
+    //     [],             // topics (optional)
+    //     ['663b9be10025fd74d015'],  
+    //     [], // targets (optional)
+    //     {
+    //       "sound" :"sound",
+    //       "channel_id": "2001"
+    //     }, 
+    //     undefined, // action
+    //     undefined, // image
+    //     undefined, // icon
+    //     "noti_sound", 
+    //     undefined, // color
+    //     undefined, // tag
+    //     undefined, // badge
+    //     undefined, // draft
+    //     undefined  // scheduledAt
+    // );
+
+    const message = {
+      data: {
+        title: "title",
+        body: "body",
+      },
+      tokens: ['dGHE_ILSTKWcEOY5m_M69E:APA91bFDNADy9kqqOAKALrVm_0U7dVfk_KKZ36gfmqZz41nRjMnEsL3a4cz065o18qYOknt8YVkOQrsTu5rn67bSC9uWwc5pWV5ltf_Y81kDI1GdczYKzToXcdtppHozBY_9GB0SYcBP'],
+    };
+
+    // Gửi thông báo
+    const response = await admin.messaging().sendMulticast(message);
+
+    console.log("OKKKKKKKKKKKKK")
+    }catch(e){
+      console.log(e)
+    }
+});
+
 
 
 app.listen(port, () => {
